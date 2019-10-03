d060474bb2b7f8f75fc53a7195db423ac9cb0ae8
Owen H | Uni <owenhowarth@gmail.com>
2019-10-03 13:00:28 +0100

    Initial Commit + Camera Movement
    Unity file has been created
    
    Camera now follows the main player

---
+++ Prototype/Prototype/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+        Quaternion playerRotation = player.transform.rotation;
+        playerPos.y += 1;
+        transform.position = playerPos;
+        transform.rotation = playerRotation;
+    }
+}
---
+++ Prototype/Prototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    Rigidbody rigidbody;
+    public float speed;
+    public float rotationSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
+        Quaternion rotation = Quaternion.Euler(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
+        transform.position += movement
+            ;
+        
+    }
+}
