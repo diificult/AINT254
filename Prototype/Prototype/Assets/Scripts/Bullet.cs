31c41d17d7f3cc4830ca21b563da6bebc4a78e30
Owen H - Owen Home <owenhowarth@gmail.com>
2019-10-14 19:48:47 +0100

    Gun now shoots
    The shootig now works on the gun

---
+++ Prototype/Prototype/Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 500f;
+    public GameObject camera;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     //   Vector3 m = new Vector3();
+        GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider other)
+    { 
+        Die();
+    }
+    private void OnBecameInvisible()
+    {
+        Die();
+    }
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Prototype/Prototype/Assets/Scripts/Gun.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public Transform bulletSpawnPoint;
+    public float fireRate = 0.5f;
+    private bool isFiring = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (!isFiring)
+            {
+                Fire();
+            }
+        }
+
+    }
+
+    private void Fire()
+    {
+        isFiring = true;
+        Instantiate(bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Invoke("SetFiring", fireRate);
+    }
+
+    private void SetFiring()
+    {
+        isFiring = false;
+    }
+
+
+}
