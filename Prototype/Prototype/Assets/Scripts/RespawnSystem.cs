83e4eacad942d35e03d44405c7bf8b9dbeb09a0f
Owen H - Owen Home <owenhowarth@gmail.com>
2019-11-02 23:55:42 +0000

    Respawn
    Working on respawn doesnt work  at the moment  need to  work  out lists<t>

--- Prototype/Prototype/Assets/Scripts/RespawnSystem.cs
+++ Prototype/Prototype/Assets/Scripts/RespawnSystem.cs
@@ -6,13 +6,33 @@ using UnityEngine.Events;
 
 public class RespawnSystem : MonoBehaviour
 {
+
+    int countdown = 5;
+    public List<GameObject> spawnLocations = new List<GameObject>();
+
     //public UnityEvent onDie;
     public UnityEvent onDamaged;
     public void Hit()
     {
         Debug.Log("Hit");
+        GetComponent<Renderer>().enabled = false;
         onDamaged.Invoke();
+        Invoke("CountdownNext", 1);
+    }
 
+    private void CountdownNext()
+    {
+        countdown--;
+        if (countdown == 0)
+        {
+      //      GameObject RespawnLocation = new GameObject();
+    //        RespawnLocation = spawnLocations[Random.Range(0, spawnLocations.GetRange)];
+   //         transform.position = ;
+   //         GetComponent<Renderer>().enabled = true;
+        } else
+        {
+            Invoke("CountdownNext", 1);
+        }
     }
 
 }
