b4dad3e64b92816505dbb402b4d8a4f8fd63bd8e
Owen H <owenhowarth@gmail.com>
2019-11-14 15:18:23 +0000

    Controller select
    The players can now press button to select the order of the controller
    There is a indication when they press A which controller they are.
    Started work on starting the game with the new controls.

---
+++ Prototype/Prototype/Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerManager : MonoBehaviour
+{
+
+    int player1 = 0;
+    int player2 = 0;
+    public Image player1Image;
+    Animator player1Animation;
+    public Image player2Image;
+    Animator player2Animation;
+
+    public Text pressStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player1Animation = player1Image.GetComponent<Animator>();
+        player2Animation = player2Image.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("J1A"))
+        {
+            Debug.Log("j2a, player1 = " + player1 + ", player2 = " + player2);
+            if (player1 == 0 && player2 != 1)
+            {
+                player1 = 1;
+                player1Image.color = Color.green;
+            }
+            else if (player1 == 1)
+            {
+                player1Animation.SetTrigger("ButtonPressed");
+            }
+            else if (player2 == 0 && player1 != 1)
+            {
+                player2 = 1;
+                player2Image.color = Color.green;
+            }
+            else if (player2 == 1)
+            {
+                player2Animation.SetTrigger("ButtonPressed");
+            }
+        }
+        if (Input.GetButtonDown("J2A"))
+        {
+            
+            Debug.Log("j2a, player1 = " + player1 + ", player2 = " + player2);
+            if (player1 == 0 && player2 != 2)
+            {
+                player1 = 2;
+                player1Image.color = Color.green;
+            }
+            else if (player1 == 2)
+            {
+                player1Animation.SetTrigger("ButtonPressed");
+            }
+            else if (player2 == 0 && player1 != 2)
+            {
+                player2 = 2;
+                player2Image.color = Color.green;
+            }
+            else if (player2 == 2)
+            {
+                player2Animation.SetTrigger("ButtonPressed");
+            }
+        }
+        if (player1 > 0 && player2 > 0)
+        {
+            pressStart.enabled = true;
+           // if ()
+        }
+
+    }
+}
--- Prototype/Prototype/Assets/Scripts/PlayerInput.cs
+++ Prototype/Prototype/Assets/Scripts/PlayerInput.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+
+    private string moveXAxis;
+    private string moveYAxis;
+    private string lookXAxis;
+    private string lookYAxis;
+    private string fire;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +27,10 @@ public class PlayerInput : MonoBehaviour
     //https://www.youtube.com/watch?v=WIZz2oiZyqU
     void SetControllerNumber(int  number)
     {
-
+        moveXAxis = "J" + number + "Horizontal";
+        moveYAxis = "J" + number + "Vertical";
+        lookXAxis = "J" + number + "StickHorizontal";
+        lookYAxis = "J" + number + "StickVertical";
+        fire = "J" + number + "fire";
     }
 }
