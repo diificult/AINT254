6dab203d9326e00865c97797ee46223613bb1db9
Owen H <owenhowarth@gmail.com>
2019-12-12 15:49:31 +0000

    Light!
    Particles are working nicer!

---
+++ Prototype/Prototype/Assets/Scripts/Partical.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Partical : MonoBehaviour
+{
+    public GameObject player;
+    private bool isStarted = false;
+
+    private void Update()
+    {
+        if (GetComponent<ParticleSystem>().isEmitting && !isStarted)
+        {
+            isStarted = true;
+            Vector3 playerPos = player.transform.position;
+            transform.position = new Vector3(playerPos.x + 0.35f, playerPos.y - 0.29f, playerPos.z + 0.47f);
+            transform.rotation = player.transform.rotation;
+        }
+        else if (!GetComponent<ParticleSystem>().isEmitting)
+        {
+            isStarted = false;
+        }
+
+    }
+
+
+}
